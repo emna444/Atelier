8f7aa620c8968df18cb184f08efc9bbb7675f1ff
emnad <emna.driss@esprit.tn>
2023-09-28 13:22:04 +0100

    first commit

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service/{name}', name: "show_service")]
+    public function showService($name): Response
+    {
+        return $this->render('service/showService.html.twig',
+         [
+            'name' => $name,
+        ]);
+    }
+    public function goToIndex(): Response
+{
+    return $this->redirectToRoute('app_home');
+}
+}
